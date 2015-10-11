78b1d05f2bd0c9b5d497df14e089493201ced20f
KFL <kfldev@outlook.com>
2015-10-10 21:01:30 -0700

    Add functional

---
+++ kfl.utils/Functional/Functional.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kfl.Utils.Functional
+{
+    public static class Functional
+    {
+        public static T Min<T>(T x, T y) where T : IComparable, IComparable<T>
+        {
+            return x.CompareTo(y) > 0 ? y : x;
+        }
+
+        public static T Max<T>(T x, T y) where T : IComparable, IComparable<T>
+        {
+            return x.CompareTo(y) > 0 ? x : y;
+        }
+    }
+}
---
+++ test/FunctionalTests.cs
@@ -0,0 +1,22 @@
+﻿namespace test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+    using static Kfl.Utils.Functional.Functional;
+
+    public class FunctionalTests
+    {
+        [Xunit.Fact]
+        public void TestMinMax()
+        {
+            Func<int, int, int> min = Min<int>;
+            Func<double, double, double> max = Max<double>;
+            Assert.Equal(1, min(1, 2));
+            Assert.Equal(2.0, max(2.0, -1));
+        }
+    }
+}
